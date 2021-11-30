41bd0af366eebe6dbe4d452e3bf4ea217e8f8423
Ishaan Billore <ishaanbillore13@gmail.com>
2021-12-01 02:58:21 +0530

    Add files via upload

---
+++ dist/index.dev.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var likecount = 0;
+$(document).ready(function () {
+  $("button").click(function (e) {
+    if (likecount % 2 == 0) {
+      likecount++;
+      $(".likecountdisplay").text(likecount);
+      $(".likes").toggleClass("liked ");
+    } else if (likecount % 2 != 0) {
+      likecount--;
+      $(".likecountdisplay").text(likecount);
+      $(".likes").toggleClass("liked ");
+    }
+
+    if (likecount == 0) {
+      $(".likecountdisplay").text("");
+    } else {
+      $(".likecountdisplay").show();
+    }
+  });
+});
---
+++ dist/main-page.dev.js
@@ -0,0 +1,5 @@
+"use strict";
+
+$(".lifestyle").click(function (e) {
+  $("div").toggleClass("display");
+});
